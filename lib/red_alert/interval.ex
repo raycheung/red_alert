34de516beca0bfe6da6793fa2388b6ab14ea3dc4
Ray Cheung <ray.swc@gmail.com>
2016-11-17 21:46:35 +0800

    Add available interval options

---
+++ lib/red_alert/interval.ex
@@ -0,0 +1,16 @@
+defmodule RedAlert.Interval do
+  @a_minute 60
+  @a_day 24 * 60 * @a_minute
+  @options [
+    every_15mins: 15 * @a_minute,
+    every_30mins: 30 * @a_minute,
+    every_1hour:  60 * @a_minute,
+    every_1day:   @a_day,
+    every_7days:  7 * @a_day,
+  ]
+  if Mix.env in ~w(dev test)a, do: @options [{:every_5secs, 5}, {:every_30secs, 30} | @options]
+
+  def options, do: Keyword.keys(@options)
+
+  def get(p), do: Keyword.get(@options, p)
+end
