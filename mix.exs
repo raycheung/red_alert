ff5135080569e24df5e271888172f4f81a83eb7a
Ray Cheung <ray.swc@gmail.com>
2016-11-18 21:44:06 +0800

    Add ex_doc

--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule RedAlert.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [{:timex, "~> 3.0"},
+     {:ex_doc, ">= 0.0.0", only: :dev},
      {:credo, "~> 0.4", only: [:dev, :test]}]
   end
 
