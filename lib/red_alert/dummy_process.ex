a5d747c3666cc15bb00c2fbea7a0ad53a0f9107a
Ray Cheung <ray.swc@gmail.com>
2016-11-19 13:19:33 +0800

    Improve dummy process

--- lib/red_alert/dummy_process.ex
+++ lib/red_alert/dummy_process.ex
@@ -1,10 +1,14 @@
 defmodule RedAlert.DummyProcess do
+  @moduledoc ~S"""
+  A dummy process to snooze the alert periodically, in a prescheduled hit and miss manner.
+  """
+
   @interval 5 * 1_000
   @hits [true, true, false]
 
-  def start, do: @hits |> Stream.cycle |> Enum.each(&work/1)
+  def cycle, do: @hits |> Stream.cycle |> Enum.each(&do_cycle/1)
 
-  def work(hit) do
+  defp do_cycle(hit) do
     Process.sleep @interval
     if hit, do: RedAlert.snooze __MODULE__
   end
